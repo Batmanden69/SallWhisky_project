d70b2c07c72ef0c4ce3eb25375114064b7d34fa7
Emil Egelund <emilmondrupjacobsen@gmail.com>
2023-03-22 08:49:23 +0100

    Lager og Plads

--- src/model/Lager.java
+++ src/model/Lager.java
@@ -1,5 +1,63 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Lager {
     private String navn;
+    private int ledigePladser;
+    private ArrayList<Fad> fadListe;
+    private ArrayList<Plads> pladsListe;
+
+    public Lager(String navn, int ledigePladser) {
+        this.navn = navn;
+        this.ledigePladser = ledigePladser;
+    }
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
+
+    public int getLedigePladser() {
+        return ledigePladser;
+    }
+
+    public void setLedigePladser(int ledigePladser) {
+        this.ledigePladser = ledigePladser;
+    }
+
+    public ArrayList<Fad> getFadListe() {
+        return new fadListe;
+    }
+
+    public void addFad(Fad fad){
+        if(!fadListe.contains(fad)){
+            fadListe.add(fad);
+            fad.setLager(this);
+        }
+    }
+
+    public void removeFad(Fad fad){
+        if(fadListe.contains(fad)){
+            fadListe.remove(fad);
+            fad.setLager(null);
+        }
+    }
+
+    public void addPlads(Plads plads){
+        if(!pladsListe.contains(plads)){
+            pladsListe.add(plads);
+            plads.setLager(this);
+        }
+    }
+
+    public void removePlads(Plads plads){
+        if(pladsListe.contains(plads)){
+            pladsListe.remove(plads);
+            plads.setLager(null);
+        }
+    }
 }
