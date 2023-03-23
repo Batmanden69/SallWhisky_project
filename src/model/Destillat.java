1592fd15da95336089980f8c219940b5fde84c9c
Jacob Gosmer <eaajago@students.dk>
2023-03-23 12:01:39 +0100

    forbindelse mellem fad og Destillat

--- src/model/Destillat.java
+++ src/model/Destillat.java
@@ -12,4 +12,36 @@ public class Destillat {
         this.destillering = destillering;
     }
 
+    public String getHistorie() {
+        return historie;
+    }
+
+    public void setHistorie(String historie) {
+        this.historie = historie;
+    }
+
+    public Destillering getDestillering() {
+        return destillering;
+    }
+
+    public void setDestillering(Destillering destillering) {
+        this.destillering = destillering;
+    }
+
+    public ArrayList<Fad> getFadList() {
+        return fadList;
+    }
+
+    public void addFad (Fad fad){
+        if (!fadList.contains(fad)){
+            fadList.add(fad);
+            fad.setDestillat(this);
+        }
+    }
+    public void removeFad(Fad fad){
+        if (fadList.contains(fad)){
+            fadList.remove(fad);
+            fad.setDestillat(null);
+        }
+    }
 }
