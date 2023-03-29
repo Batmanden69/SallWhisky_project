281b7aa3d15b796ae363c341052031f709cf09e8
Jens Laugaard Pedersen <eaajenlp@students.eaaa.dk>
2023-03-29 14:34:00 +0200

    Controller og storage

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,8 +1,25 @@
 package controller;
 
+import storage.Storage;
+
 public class Controller {
 
+    private static Controller instance;
 
+    public Controller() {
+    }
+
+    public static Controller getInstance() {
+        if (instance == null) {
+            instance = new Controller();
+        }
+        return instance;
+    }
+
+
+    public void initStorage() {
+        
+    }
 
 
 }
